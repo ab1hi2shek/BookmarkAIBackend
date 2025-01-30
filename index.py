141ba7258345ca0ae5b7a83fc8b4efd757ad6422
ab1hi2shek <ab1hi2shek@gmail.com>
2025-01-30 00:35:17 -0800

    versel 2

--- index.py
+++ index.py
@@ -7,4 +7,5 @@ app = create_app()
 app.register_blueprint(api_blueprint, url_prefix="/api")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
